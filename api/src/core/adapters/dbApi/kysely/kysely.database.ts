d32a3cd782094d5d52c42baad340d0cbcfbcdf34
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2026-03-03 14:57:17 +0100

    feat: add new table for author organization

--- api/src/core/adapters/dbApi/kysely/kysely.database.ts
+++ api/src/core/adapters/dbApi/kysely/kysely.database.ts
@@ -86,6 +86,7 @@ export type Database = {
     sources: SourcesTable;
     user_sessions: SessionsTable;
     software_attribute_definitions: SoftwareAttributeDefinitionsTable;
+    author_organizations: AuthorOrganizationsTable;
 };
 
 type UsersTable = {
@@ -245,6 +246,11 @@ type SessionsTable = {
     createdAt: Date;
     updatedAt: Date;
     loggedOutAt: Date | null;
+};
+
+type AuthorOrganizationsTable = {
+    id: string;
+    organization: SchemaOrganization;
 };
 
 // ---------- compiled data ----------
