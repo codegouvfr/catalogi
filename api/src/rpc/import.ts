123e9b91970ad7680e6de776e4d0b86c7598be63
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2026-09-10 11:41:07 +0200

    feat: add command line usage for import refs: #577

--- api/src/rpc/import.ts
+++ api/src/rpc/import.ts
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2021-2025 DINUM <floss@numerique.gouv.fr>
-// SPDX-FileCopyrightText: 2024-2025 Université Grenoble Alpes
+// SPDX-FileCopyrightText: 2021-2026 DINUM <floss@numerique.gouv.fr>
+// SPDX-FileCopyrightText: 2024-2026 Université Grenoble Alpes
 // SPDX-License-Identifier: MIT
 
 import { Kysely } from "kysely";
@@ -10,13 +10,20 @@ import { createPgDialect } from "../core/adapters/dbApi/kysely/kysely.dialect";
 import { importTool } from "../core/importTool";
 
 export async function startImportService(params: {
-    isDevEnvironnement: boolean;
-    databaseUrl: string;
-    botUserEmail?: string;
-    importDataSourceOrigin: string;
-    listToImport?: string[];
+    env: {
+        isDevEnvironnement: boolean;
+        databaseUrl: string;
+        botUserEmail?: string;
+        importDataSourceOrigin: string;
+        listToImport?: string[];
+    };
+    args: {
+        sourceSlug?: string;
+        externalIdsToImport?: string[];
+    };
 }) {
-    const { isDevEnvironnement, databaseUrl, botUserEmail, listToImport, importDataSourceOrigin, ...rest } = params;
+    const { isDevEnvironnement, databaseUrl, botUserEmail, listToImport, importDataSourceOrigin, ...rest } = params.env;
+    const { sourceSlug: argSourceSlug, externalIdsToImport: argExternalIdsToImport } = params.args;
 
     assert<Equals<typeof rest, {}>>();
 
@@ -24,14 +31,19 @@ export async function startImportService(params: {
 
     const kyselyDb = new Kysely<Database>({ dialect: createPgDialect(databaseUrl) });
 
+    const parsedList =
+        argExternalIdsToImport?.length && argExternalIdsToImport.length === 1
+            ? argExternalIdsToImport[0].split(",")
+            : undefined;
+
     const success = await importTool({
         "dbConfig": {
             "dbKind": "kysely",
             "kyselyDb": kyselyDb
         },
         "botUserEmail": botUserEmail,
-        "sourceSlug": importDataSourceOrigin,
-        "listToImport": listToImport ?? []
+        "sourceSlug": argSourceSlug ?? importDataSourceOrigin,
+        "listToImport": parsedList ?? argExternalIdsToImport ?? listToImport ?? []
     });
 
     success ? console.info("[RPC:Import] ✅ Importation successful ✅") : console.error("[RPC:Import] ❌ Error ❌");
