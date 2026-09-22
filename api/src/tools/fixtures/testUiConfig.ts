cfa3ee81dd6a38bfe8ef7c95e05b79c37bf74c2c
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2026-09-22 15:51:38 +0200

    chore: add config otpion devorgs

--- api/src/tools/fixtures/testUiConfig.ts
+++ api/src/tools/fixtures/testUiConfig.ts
@@ -18,6 +18,7 @@ export const testUiConfig = uiConfigSchema.parse({
             catalog: { enabled: true },
             addSoftware: { enabled: true },
             about: { enabled: true },
+            devOrganizations: { enabled: false },
             contribute: {
                 enabled: true,
                 href: "mailto:floss@numerique.gouv.fr?subject=Demande d'accompagnement"
