d869d49fb0a91cd110ca6e97ab9df881cff92c66
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2026-09-25 12:08:50 +0200

    refactor: handle new config

--- api/src/tools/fixtures/testUiConfig.ts
+++ api/src/tools/fixtures/testUiConfig.ts
@@ -19,6 +19,7 @@ export const testUiConfig = uiConfigSchema.parse({
             addSoftware: { enabled: true },
             about: { enabled: true },
             devOrganizations: { enabled: false },
+            documentation: { enabled: true, href: "doc.gouv.fr" },
             contribute: {
                 enabled: true,
                 href: "mailto:floss@numerique.gouv.fr?subject=Demande d'accompagnement"
